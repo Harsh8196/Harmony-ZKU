fdc99eb56d4d8798b88df344bf08948d7967be84
HarshMashruwala <harshmashruwala8196@gmail.com>
2022-03-20 20:30:48 +0530

    Week2Assignment

---
+++ Week2 Assignment/custom.test.js
@@ -0,0 +1,160 @@
+const hre = require('hardhat')
+const { ethers, waffle } = hre
+const { loadFixture } = waffle
+const { expect } = require('chai')
+const { utils } = ethers
+
+const Utxo = require('../src/utxo')
+const { transaction, registerAndTransact, prepareTransaction, buildMerkleTree } = require('../src/index')
+const { toFixedHex, poseidonHash2 } = require('../src/utils')
+const { Keypair } = require('../src/keypair')
+const { encodeDataForBridge } = require('./utils')
+
+const MERKLE_TREE_HEIGHT = 5
+const l1ChainId = 1
+const MINIMUM_WITHDRAWAL_AMOUNT = utils.parseEther(process.env.MINIMUM_WITHDRAWAL_AMOUNT || '0.05')
+const MAXIMUM_DEPOSIT_AMOUNT = utils.parseEther(process.env.MAXIMUM_DEPOSIT_AMOUNT || '1')
+const MerkleTree = require('fixed-merkle-tree')
+
+describe('TornadoPool-custom Js', function () {
+  this.timeout(20000)
+
+  async function deploy(contractName, ...args) {
+    const Factory = await ethers.getContractFactory(contractName)
+    const instance = await Factory.deploy(...args)
+    return instance.deployed()
+  }
+
+  async function fixture() {
+    require('../scripts/compileHasher')
+    const [sender, gov, l1Unwrapper, multisig] = await ethers.getSigners()
+    const verifier2 = await deploy('Verifier2')
+    const verifier16 = await deploy('Verifier16')
+    const hasher = await deploy('Hasher')
+
+    const token = await deploy('PermittableToken', 'Wrapped ETH', 'WETH', 18, l1ChainId)
+    await token.mint(sender.address, utils.parseEther('10000'))
+
+    const amb = await deploy('MockAMB', gov.address, l1ChainId)
+    const omniBridge = await deploy('MockOmniBridge', amb.address)
+
+    /** @type {TornadoPool} */
+    const tornadoPoolImpl = await deploy(
+      'TornadoPool',
+      verifier2.address,
+      verifier16.address,
+      MERKLE_TREE_HEIGHT,
+      hasher.address,
+      token.address,
+      omniBridge.address,
+      l1Unwrapper.address,
+      gov.address,
+      l1ChainId,
+      multisig.address,
+    )
+
+    const { data } = await tornadoPoolImpl.populateTransaction.initialize(
+      MINIMUM_WITHDRAWAL_AMOUNT,
+      MAXIMUM_DEPOSIT_AMOUNT,
+    )
+    const proxy = await deploy(
+      'CrossChainUpgradeableProxy',
+      tornadoPoolImpl.address,
+      gov.address,
+      data,
+      amb.address,
+      l1ChainId,
+    )
+
+    const tornadoPool = tornadoPoolImpl.attach(proxy.address)
+
+    await token.approve(tornadoPool.address, utils.parseEther('10000'))
+
+    return { tornadoPool, token, proxy, omniBridge, amb, gov, multisig }
+  }
+
+  function getNewTree() {
+    return new MerkleTree(MERKLE_TREE_HEIGHT, [], { hashFunction: poseidonHash2 })
+  }
+
+  async function fixtureTree() {
+    require('../scripts/compileHasher')
+    const hasher = await deploy('Hasher')
+    const merkleTreeWithHistory = await deploy(
+      'MerkleTreeWithHistoryMock',
+      MERKLE_TREE_HEIGHT,
+      hasher.address,
+    )
+    await merkleTreeWithHistory.initialize()
+    return { hasher, merkleTreeWithHistory }
+  }
+
+
+  it('Estimate Gas , deposit from L1 and withdraw to L2', async function () {
+
+    const { merkleTreeWithHistory } = await loadFixture(fixtureTree)
+    const tree = getNewTree()
+    const gas = await merkleTreeWithHistory.estimateGas.insert(toFixedHex(123), toFixedHex(456))
+    console.log('Estimate Gas--', gas.toString())
+    tree.bulkInsert([123, 456])
+
+
+    const { tornadoPool, token, omniBridge } = await loadFixture(fixture)
+    const aliceKeypair = new Keypair() // contains private and public keys
+
+    // Alice deposits into tornado pool
+    const aliceDepositAmount = utils.parseEther('0.08')
+    const aliceDepositUtxo = new Utxo({ amount: aliceDepositAmount, keypair: aliceKeypair })
+    const { args, extData } = await prepareTransaction({
+      tornadoPool,
+      outputs: [aliceDepositUtxo],
+    })
+
+    const onTokenBridgedData = encodeDataForBridge({
+      proof: args,
+      extData,
+    })
+
+    const onTokenBridgedTx = await tornadoPool.populateTransaction.onTokenBridged(
+      token.address,
+      aliceDepositUtxo.amount,
+      onTokenBridgedData,
+    )
+    // emulating bridge. first it sends tokens to omnibridge mock then it sends to the pool
+    await token.transfer(omniBridge.address, aliceDepositAmount)
+    const transferTx = await token.populateTransaction.transfer(tornadoPool.address, aliceDepositAmount)
+
+    await omniBridge.execute([
+      { who: token.address, callData: transferTx.data }, // send tokens to pool
+      { who: tornadoPool.address, callData: onTokenBridgedTx.data }, // call onTokenBridgedTx
+    ])
+
+    // withdraws a part of his funds from the shielded pool
+    const aliceWithdrawAmount = utils.parseEther('0.05')
+    const recipient = '0xDeaD00000000000000000000000000000000BEEf'
+    const aliceChangeUtxo = new Utxo({
+      amount: aliceDepositAmount.sub(aliceWithdrawAmount),
+      keypair: aliceKeypair,
+    })
+    await transaction({
+      tornadoPool,
+      inputs: [aliceDepositUtxo],
+      outputs: [aliceChangeUtxo],
+      recipient: recipient,
+      isL1Withdrawal: false,
+    })
+
+    const recipientBalance = await token.balanceOf(recipient)
+    // console.log(recipientBalance.toString())
+    expect(recipientBalance).to.be.equal(aliceWithdrawAmount)
+    const omniBridgeBalance = await token.balanceOf(omniBridge.address)
+    // console.log(omniBridgeBalance.toString())
+    expect(omniBridgeBalance).to.be.equal(0)
+    
+    const tornadoPoolBalance = await token.balanceOf(tornadoPool.address)
+    // console.log(tornadoPoolBalance.toString())
+    expect(tornadoPoolBalance).to.be.equal(utils.parseEther('0.03'))
+  })
+
+
+})
