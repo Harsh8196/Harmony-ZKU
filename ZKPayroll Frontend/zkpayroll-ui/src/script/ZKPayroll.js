eb5ed6938ede00e18c7a3717a537b31ae57bf6d4
HarshMashruwala <harshmashruwala8196@gmail.com>
2022-05-07 18:16:52 +0530

    Mainnet Launch

--- ZKPayroll Frontend/zkpayroll-ui/src/script/ZKPayroll.js
+++ ZKPayroll Frontend/zkpayroll-ui/src/script/ZKPayroll.js
@@ -2,7 +2,7 @@ import web3 from './web3_'
 import ZKPayrollAbi from '../abi/ZKPayroll.json'
 
 
-//const instance = new web3.eth.Contract(ZKPayrollAbi.abi,'0x4F61dDbd8073eb16c257A4329ECB1BDAF95585E0')  Testnet Contract
+//const instance = new web3.eth.Contract(ZKPayrollAbi.abi,'0x6999bA844230435e15cC31d9251577dAeEb9fD35')  Testnet Contract
 // console.log(instance)
 const instance = new web3.eth.Contract(ZKPayrollAbi.abi,'0x9BBB0C1f4C088260B368277fBC161Aac93e1c21c')  //Mainnet Contract
 
